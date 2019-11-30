f8a7d3f6adec2cc65793fe5ecfcc124b9276c1ab
sorvihead <novoid86@yandex.ru>
2019-11-30 19:09:07 +0300

    create_endpoints Fix index view

--- microservice/views.py
+++ microservice/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-from microservice.models import Microservice
+from microservice.models import Update
 
 
 def index(request):
-    microservices = Microservice.objects.all()[:10]
+    microservices = Update.objects.order_by(Update.date).all()[:10]
     return render(template_name='index', context={'microservices': microservices}, request=request)
 
